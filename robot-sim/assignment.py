ecbacfffa04391b93df0aebfdc8a07c9307658b6
Tabi43_ROS_M <marcorda28@gmail.com>
2022-10-23 18:21:06 +0000

    New idea assignment2

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -8,8 +8,12 @@ from sr.robot import *
 
 
 R = Robot()
+
 silver_token = []
 gold_token = []
+
+a_th = 2.0
+d_th = 0.4
 
 def drive(speed, seconds):
     """
@@ -37,8 +41,6 @@ def turn(speed, seconds):
     R.motors[0].m0.power = 0
     R.motors[0].m1.power = 0
     
-a_th = 2.0
-d_th = 0.4
 
 def find_block(type):
 	print("I'm searching the nearest...")
@@ -53,58 +55,23 @@ def find_block(type):
 			#print("token ->",m.dist,m.rot_y,m.info.code,m.info.marker_type)	
 			if(m.info.marker_type == type):			
 				if(m.dist < dist or dist == 0):
-					if(check_used_silver_token(m.info.code) and (type == "silver-token")):
-						print("token already used!")
-					else:
+					if(type == "silver-token" and contains(m.info.code, silver_token) == -1):
 						dist = m.dist
 						rot_y = m.rot_y
 						code = m.info.code
+					if(type == "gold-token" and contains(m.info.code, gold_token) == -1):
+						dist = m.dist
+						rot_y = m.rot_y
+						code = m.info.code	
 		i += 1
 		turn(10,1)
 	if(dist == 0):
 		print("There is no token of type: ", type)
 	else:
 		print("Founded a token distant: ", dist, " angle: " , rot_y , "code: ", code) 
-		return code
-		
-			
-def grab_block_code(code, type):
-	print("Going to grab the token with code :", code)
-	markers = R.see()
-	dist = 0
-	while(dist == 0):
-		turn(10,1)
-		markers = R.see()
-		for m in markers:
-			if(m.info.code == code and m.info.marker_type == type):
-				rot_y = m.rot_y	
-				dist = m.dist
-				print("I can see it!")
-	for m in markers:
-		if(m.info.code == code and m.info.marker_type == type):
-			rot_y = m.rot_y
-			dist = m.dist
-			while(dist > d_th):
-				if(-a_th<= rot_y <= a_th): 
-					print("Ah, here we are!.")
-					drive(15, 0.5)
-				elif(rot_y < -a_th): 
-					print("Left a bit...")
-					turn(-2, 0.5)
-				elif(rot_y > a_th):
-					print("Right a bit...")
-					turn(+2, 0.5)
-				#Update of the relative position to markers
-				markers = R.see()
-				for m in markers:
-					if(m.info.code == code and m.info.marker_type == type):
-						rot_y = m.rot_y	
-						dist = m.dist					
-			check = R.grab()
-			return check
-				
+		return code				
 	
-def reach_block_code(code, type):
+def reach_block_code(code, type, m_dst):
 	print("Going to reach the token with code :", code)
 	markers = R.see()
 	dist = 0
@@ -120,7 +87,7 @@ def reach_block_code(code, type):
 		if(m.info.code == code and m.info.marker_type == type):
 			rot_y = m.rot_y
 			dist = m.dist
-			while(dist > 1.5*d_th):
+			while(dist > m_dst):
 				if(-a_th<= rot_y <= a_th): 
 					print("Ah, here we are!.")
 					drive(20, 0.5)
@@ -133,26 +100,27 @@ def reach_block_code(code, type):
 				#Update of the relative position to markers
 				markers = R.see()
 				for m in markers:
-					if(m.info.code == code and m.info.marker_type == type):
-						rot_y = m.rot_y	
-						dist = m.dist				
-	
-def check_used_silver_token(code):
-	for c in silver_token:
-		print("c = ",c)
+					if(m.info.code == code):
+						if(m.info.marker_type == type):
+							rot_y = m.rot_y	
+							dist = m.dist				
+			
+def contains(code, list):
+	i = 0
+	for c in list:
 		if(code == c):
-			return 1
-	return 0
+			return i
+	return -1
 	
-while(1):
-	os.system('clear')
+os.system('clear')
+while(1):	
 	code = find_block("silver-token")
-	check = grab_block_code(code,"silver-token")
-	if(check == 1):
-		print("silver-token code: ",code," added...")
-		silver_token.append(code)
+	next_token = reach_block_code(code, "silver-token", d_th)
+	R.grab()
+	silver_token.append(code)
 	code = find_block("gold-token")
-	reach_block_code(code,"gold-token")
+	next_token = reach_block_code(code, "gold-token", 1.5*d_th)
 	R.release()
+	gold_token.append(code)
 	drive(-10,2)
 
